b079cc0df81f0c7e4695bfb9dc4f346ba6b7f652
Hemanth varma <varmahemanth82@gmail.com>
2020-04-03 22:58:50 +0530

    dassa

--- EAD/routes/user.js
+++ EAD/routes/user.js
@@ -182,12 +182,17 @@ console.log(req.isAuthenticated());
           }
           // console.log(gen)
           return res.render('dashboard',{user:req.user.email,genre:lis_genre,books:gen,layout:'navbar2.ejs'})
-
-        })
-        // return res.sendStatus(200)
+        })  // return res.sendStatus(200)
      }
-
 })
+
+router.get('/seebooks/:genre',(req,res)=>{
+  Book.find({Genre:req.params.genre}).then(x=>{
+    // console.log(x[0])
+    return res.render('genrebooks',{books:x,layout:'navbar2.ejs'})
+  })
+})
+
 
 router.get('/profile',(req,res)=>{
 
