0160754ef44183df9fcb891de0008c4699939836
Narendra186 <40228592+Narendra186@users.noreply.github.com>
2020-06-13 09:59:27 +0530

    payment checks

---
+++ EAD/models/Payment.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Book = require('./Book')
+const User = require('./User')
+
+const PaymentSchema = mongoose.Schema({
+  owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  purchaser:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  book:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Book'
+  },
+  status:{
+    type:Boolean,
+    default:false
+  },
+  amount:{
+    type:Number,
+  },
+  date:{
+    type:Date,
+    default:Date.now
+  },
+
+})
+
+const Payment = mongoose.model('Payment',PaymentSchema);
+
+
+module.exports = Payment;
