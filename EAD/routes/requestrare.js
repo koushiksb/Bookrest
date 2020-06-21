dd3746af24778091cd16c2c8657190496777e256
Koushik S B <koushik@users.noreply.github.com>
2020-06-21 11:39:56 +0530

    added nav pills

--- EAD/routes/requestrare.js
+++ EAD/routes/requestrare.js
@@ -74,18 +74,50 @@ console.log(x);
   return res.render('dummyviewrequest',{requests:x,layout:'navbar2'})
 })
 })
-// router.get('/viewsentrequest',(req,res)=>{
-//
-// console.log('found');
-// RareRequest.find({requester:req.user.id}).populate({path:'recipient',model:'User',populate:{path:'profile',model:'Profile'}}).populate('book','Title ImageURLL Author').then(x=>{
-// // RareRequest.find({recipient:req.user.id,status:0}).populate('book','Title ImageURLS').then(x=>{
-// console.log(x);
-// var y = x.recipient;
-// x.recipient = x.requester;
-// y=x.requester
-//   return res.render('sentRequests',{requests:x,layout:'navbar2'})
-// })
-// })
+router.get('/viewsentrequest',(req,res)=>{
+
+console.log('found');
+RareRequest.find({requester:req.user.id,status:'0'}).populate({path:'recipient',model:'User',populate:{path:'profile',model:'Profile'}}).populate('book','Title ImageURLL Author').then(async (x)=>{
+// RareRequest.find({recipient:req.user.id,status:0}).populate('book','Title ImageURLS').then(x=>{
+console.log(x);
+console.log('exchange............');
+for(var i = 0; i <  x.length; i++) {
+  var y = x[i].recipient
+  x[i].recipient = x[i].requester;
+  x[i].requester =y;
+
+}
+console.log(x);
+  return res.render('sentRequests',{requests:x,layout:'navbar2'})
+// return res.sendStatus(200)
+})
+})
+router.get('/oldrequests',(req,res)=>{
+  RareRequest.find({requester:req.user.id,status:{$ne:'0'}}).populate({path:'recipient',model:'User',populate:{path:'profile',model:'Profile'}}).populate('book','Title ImageURLL Author').then(async (x)=>{
+  // RareRequest.find({recipient:req.user.id,status:0}).populate('book','Title ImageURLS').then(x=>{
+  console.log(x);
+  console.log('exchange............');
+  for(var i = 0; i <  x.length; i++) {
+    var y = x[i].recipient
+    x[i].recipient = x[i].requester;
+    x[i].requester =y;
+
+  }
+  console.log(x);
+    return res.render('oldrequests',{requests:x,layout:'navbar2'})
+  // return res.sendStatus(200)
+  })
+
+})
+
+router.post('deleterequest',(req,res)=>{
+  RareRequest.findOneAndDelete({_id:req.body._id}).then(x=>{
+    res.sendStatus(200);
+  })
+  .catch(err=>{
+    res.sendStatus(500)
+  })
+})
 
 router.post('/viewrequest',(req,res)=>{
   console.log('posted');
